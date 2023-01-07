9717827c4800fcc6c624f63a1ef571cd55fd27a3
abhirampjayan <abhiramp08@gmail.com>
2023-01-07 23:31:27 +0530

    room slice created

---
+++ src/config/index.ts
@@ -0,0 +1,3 @@
+const VIDEO_TOKEN = process.env.VIDEO_TOKEN || '';
+
+export { VIDEO_TOKEN };
--- src/store/slices/roomSlice.ts
+++ src/store/slices/roomSlice.ts
@@ -1,22 +1,48 @@
-import { InitialEntry } from './../../../node_modules/@remix-run/router/history';
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
-import { Room } from 'twilio-video';
+import { connect, Room } from 'twilio-video';
+import { VIDEO_TOKEN } from '../../config';
+import { RootState } from '../store';
 
 interface InitialState {
   room: Room | null;
+  error: string | null;
+  status: 'successful' | 'failed' | 'connecting' | 'idle';
 }
 const initialState: InitialState = {
   room: null,
+  status: 'idle',
+  error: null,
 };
 
-const connectToRoom = createAsyncThunk('room/Connect', async (InitialEntry) => {});
+export const connectToRoom = createAsyncThunk(
+  'room/Connect',
+  async (room: string) => {
+    const data = await connect(VIDEO_TOKEN, { name: room });
+    return data;
+  }
+);
 
 export const roomSlice = createSlice({
   name: 'room',
   initialState,
   reducers: {},
-  extraReducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(connectToRoom.pending, (state) => {
+        state.status = 'connecting';
+      })
+      .addCase(connectToRoom.fulfilled, (state, action) => {
+        state.status = 'successful';
+        state.room = action.payload;
+      })
+      .addCase(connectToRoom.rejected, (state, action) => {
+        state.status = 'successful';
+        state.error = action.error.message || 'Error occurred while connecting';
+      });
+  },
 });
 
 export const {} = roomSlice.actions;
 export default roomSlice.reducer;
+
+export const getRoom = (state: RootState) => state.room.room;
