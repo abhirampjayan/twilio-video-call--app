06f6d32155ce1514f65a82d68e0102fc4fb089c5
abhirampjayan <abhiramp08@gmail.com>
2023-01-13 09:05:50 +0530

    done

--- src/components/AudioTrack.tsx
+++ src/components/AudioTrack.tsx
@@ -11,9 +11,6 @@ export default function AudioTrack({ track }: AudioTrackProps) {
   const audioEl = useRef<HTMLAudioElement>();
 
   useEffect(() => {
-    console.log('====================================');
-    console.log(track);
-    console.log('====================================');
     audioEl.current = track.attach();
     audioEl.current.setAttribute('data-cy-audio-track-name', track.name);
     document.body.appendChild(audioEl.current);
